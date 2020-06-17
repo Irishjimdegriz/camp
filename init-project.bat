866589a7d16f93f6b700592868c468a8032c4405
Eugene Zaznoba <irishjimdegriz@gmail.com>
2020-06-17 23:07:45 +0300

    feat: init project + header + hero (no styles)

---
+++ init-project.bat
@@ -0,0 +1,10 @@
+call npm i -d gulp
+call npm i -d gulp-cli
+call npm i -d browser-sync
+call npm i -d sass-css
+call npm i -d gulp-autoprefixer
+call npm i -d gulp-sass
+call npm i -d gulp-clean-css
+call npm i -d gulp-minify
+call npm i -d gulp-htmlmin
+call npm init
